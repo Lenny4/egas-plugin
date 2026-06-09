325dcebd3ae434f8a2f607f0226ddedc37cc3e80
lenny4 <alexandre.beaujour@hotmail.fr>
2026-06-09 16:49:13 +0200

    fix fatal error if (is_array($pReglements)) {

--- public/plugins/egas/includes/resources/FDocenteteResource.php
+++ public/plugins/egas/includes/resources/FDocenteteResource.php
@@ -60,12 +60,14 @@ class FDocenteteResource extends Resource
             // endregion
             // region pReglements
             $pReglements = GraphqlService::getInstance()->getPReglements();
-            usort($pReglements, function (stdClass $a, stdClass $b): int {
-                $word = 'carte';
-                similar_text($a->rIntitule, $word, $percA);
-                similar_text($b->rIntitule, $word, $percB);
-                return $percB <=> $percA;
-            });
+            if (is_array($pReglements)) {
+                usort($pReglements, function (stdClass $a, stdClass $b): int {
+                    $word = 'carte';
+                    similar_text($a->rIntitule, $word, $percA);
+                    similar_text($b->rIntitule, $word, $percB);
+                    return $percB <=> $percA;
+                });
+            }
             $pReglementsOptions = [];
             $defaultPReglement = "";
             if (!empty($pReglements)) {
