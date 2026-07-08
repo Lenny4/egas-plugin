517288ed58de5ed8ef8a8005d5070bd9bc3b61ba
lenny4 <alexandre.beaujour@hotmail.fr>
2026-07-08 19:00:09 +0200

    loginSec_disableApplicationPasswords

--- public/plugins/egas/egas.php
+++ public/plugins/egas/egas.php
@@ -3,7 +3,7 @@
  * Plugin Name: Egas – Sage Synchronization Tool
  * Plugin URI: https://egas-solutions.com/
  * Description: A plugin to use Sage on your WordPress website.
- * Version: 1.4.4
+ * Version: 1.4.5
  * Author: Egas Solutions
  * Author URI: https://egas-solutions.com/
  *
